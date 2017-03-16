a8fcdda130e3a845b609a6527507c2bc4b96e050
pjb12147 <pjb12147@arrol-29.cis.strath.ac.uk>
2017-03-16 19:58:27 +0000

    comments

--- wadtesting/wilcoxon.js
+++ wadtesting/wilcoxon.js
@@ -1,7 +1,9 @@
 //Your Client ID can be retrieved from your project in the Google
 // Developer Console, https://console.developers.google.com
+
 var CLIENT_ID = '281296927473-dru253jk83in453051u1t97f0ckdktom.apps.googleusercontent.com';
 
+//NOTE** the scope is specific. In order to read-write , the scope must be as such or else the application will simply not be able to read or write.
 var SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 
 var sheetID = "YO";
@@ -16,8 +18,11 @@ var tableRangeWilcoxon = "wilcoxontable!A";
 
 var allTheNumbers = [];
 
-function wilcoxonRun() {
 
+//This is the initial entry point for the file. This function is being calld by GUI->homeCtrl->ThisMethod
+//The method is setting up values in order to read a specific spreadsheet.->sheetID. The method also performs authorisation checks.
+function wilcoxonRun() {
+    
     sheetID = (document.getElementById("userInput").value);
     console.log(sheetID);
     gapi.auth.authorize(
@@ -29,7 +34,7 @@ function wilcoxonRun() {
 
 }
 
-
+//handles authorisation to a Google Account
 function wilcoxonhandleAuthResult(authResult) {
     var authorizeDiv = document.getElementById('authorize-div');
     if (authResult && !authResult.error) {
@@ -44,7 +49,7 @@ function wilcoxonhandleAuthResult(authResult) {
     }
 }
 
-
+//handles authorisation to a Google Account
 function wilcoxonhandleAuthClick(event) {
     gapi.auth.authorize(
             {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
@@ -52,31 +57,41 @@ function wilcoxonhandleAuthClick(event) {
     return false;
 }
 
-
+//This is the start of doing the wilcoxon calculation.
 function wilcoxonloadSheetsApi() {
 
     gapi.client.load(discoveryUrl).then(wilcoxonReadSheet);
 
 }
 
+//function is used to read the a specific Google Sheet from a spreadsheet. 
 function wilcoxonReadSheet() {
-
-
+    
+    
     var lastLetterLower = (document.getElementById("userLastColumnLetters").value);
+    //this is making sure that the last letter is in uppercase as the API seems to not accept lower case values. a simple but needed line of code.
     var lastLetters = lastLetterLower.toUpperCase();
     var sheetName = (document.getElementById("sheetName").value);
-
+    
+    //Here we are constructing the search range that we need to search on the specific sheet name and how many columns we should search up to. 
     rangeInput = sheetName + '!A2:' + lastLetters;
-
+    
+    
+    //here we make our first Google API call. 
+    //values.get is a specific call to read values from a spreadsheet. The results are then evakualuated using the .result method on the response. 
+    //the spreadsheetID is the ID you can commonly find in the URL of any Google Spreadsheet.
+    //the range, constructed from before, is used to read a specific google sheet at a certain range. This is in an A1 spreadsheet format.
     gapi.client.sheets.spreadsheets.values.get({
         spreadsheetId: sheetID,
         range: rangeInput,
         majorDimension: "COLUMNS",
     }).then(function (response) {
         var range = response.result;
+        //Console logs are in place for easy error checking and value checking.
         console.log('***********');
         console.log(range);
         console.log('***********');
+        //we then call to a different method while also passing the response from the google api call.
         wilcoxonJSON(range);
     });
 }
@@ -88,28 +103,38 @@ function wilcoxonappendPre(message) {
     pre.appendChild(textContent);
 }
 
+//here we are making a request to the wilcoxon php file. Note, the majority of the statsitcal calculations are performed server side.
+// the post request is calling on the wilcoxon.php file.....and is passing a a json encoded object. The object passed in the result from the previous api call in the method
 function wilcoxonJSON(input) {
-
+    
+    //this is the request to the php file while passing the param{param}
     $.post('wilcoxon.php', {in1: JSON.stringify(input)},
             function (data)
             {
+                //this section aims to unpackage the results from the wilcoxon.php.
+                //at this point we can consider that most of the hardcore calculation have been performed but we still need to interpret the result
+                //the json payload from the php is unpackaged into a readible format
                 var json = data;
                 obj = JSON.parse(json);
                 obj = JSON && JSON.parse(json) || $.parseJSON(json);
-//                console.log(obj.data[0][1]);
                 console.log(obj.data);
-                // var across = (obj.data[0][1]);
+                
+                //given the hardcore calculations have been performed we can now consider to create objects on the spreadsheet to represrent our findings to the user
+                //note we are passing out calculation findings as a parameter to our new function.
                 gapi.client.load(discoveryUrl).then(wilcoxoncreateGoogleObjects(obj));
-
-
+                
             });
 
 }
+//this function aims to create a new google sheet so that we have a blank sheet to paste our findings
 
 function wilcoxoncreateGoogleObjects(obj) {
-    console.log('asdfsdfsdfsdfsdf');
+    
+    //this aims to create a name of the new sheet we are about to create. we take the name of the original sheet and append the name of the test we are performing.
     var nameOfSheet = (document.getElementById("sheetName").value);
     nameOfSheet += "-Wilcoxon-Test-Results";
+    //batchupdate is the api call that allows our application to create a new sheet on the sheetID
+    // we can set the properties like row count and tab colour etc. 
     gapi.client.sheets.spreadsheets.batchUpdate({
         spreadsheetId: sheetID,
         requests: [
@@ -135,9 +160,17 @@ function wilcoxoncreateGoogleObjects(obj) {
         console.log('1');
     });
 
+
+    //basic error checking on the console to speed up development time
     console.log(obj.data[1][1]);
     console.log(tableRangeWilcoxon + obj.data[1][1]);
 
+    // the following section of code aims to read from a specific value table to find a critical value
+    // the value from the wilcoxon value is then compared to a value found on the table to state decide if there are significances.
+    //note the setTimout function** -> we must set a time delay because by the time we want to add values onto our newly created sheet...the sheet does not excist.
+    // Therefore we must put a short time delay to allow the google api to create the google sheet first. 
+    
+    
     gapi.client.sheets.spreadsheets.values.get({
         spreadsheetId: '1qgFlb_FZKpi5qQQdetCjxVf9FCEYiTLsAz463hH3uJ4',
         range: tableRangeWilcoxon + obj.data[1][1],
@@ -152,14 +185,14 @@ function wilcoxoncreateGoogleObjects(obj) {
 
         var delayMillis = 2000;
         setTimeout(function () {
+            //after the time delay has passed (which allows google time to create the sheet, we can then display out results onto the sheet that we have created.
             displayResultsWilcoxon(valueFromWilcoxonTable,valueW)
 
         }, delayMillis);
-
-
-
     });
 }
+
+//this simply displays the results that we have found onto the google sheet. 
 function displayResultsWilcoxon(tableVal,valueWin) {
     
     var nameOfSheet = (document.getElementById("sheetName").value);
--- wadtesting/wilcoxon.php
+++ wadtesting/wilcoxon.php
@@ -1,28 +1,31 @@
 <?php
+//<!--The Wilcoxon.php file is used to calculate the W value. It does this
+//by utlising the wilcoxonCalc.jar file that is infact an executable java jar file.-->
 
+
+
+//this is the input into the file by the json call from the wilcoxon.js file. this is an object read from the google sheets spreadsheet
 $input1 = $_POST['in1'];
 
+//since the input was passeed from the wilcoxon.js file was encoded in json...we must decode the json into a usable format.
 $object = json_decode($input1, true);
 
+//the sizeOf function gets the size of the [values] array within the decoded object. This value is used for processing and creating a string with a ! del between.
 $length = sizeof($object["values"]);
 array_push($object["values"][0], "!");
-//$testIN= "fgsdfgsfg";
-//$testIN1= "spicyspicy";
-//
-//$array1 =array (40,65,53,79,87,41,80,63,51,82,27,71,29);
-//$array2 =array (45,68,47,75,88,60,77,69,60,88,30,73,35);
-//
 $merged = array_merge($object["values"][0],$object["values"][1]);
 $stringToPass = implode(" ", $merged);
 
 
-//exec("java -jar NewTest.jar $array1 $array2" , $output);
-
+//the exec function is a function that can be used to call other files from different languages i.e, java, c or R. In this case
+//we are calling an exectuable java jar file that can return a W value from the running of a wilcoxon rank test. please refer to the java file for further documentation
+//the variable $output is a variable that is populated instantly with the returned value from running the jar file
 exec("java -jar wilcoxonCalc.jar $stringToPass" , $output);
 
-//exec('java -jar NewTest.jar testIN' , $output);
 
-
+// here we are setting up a stdclass. The stdClass is simply a plain vanilla class within nothing in it, basically an empty object that can be manipulated
+// we then file that stdClass with values in an array structure and echo back to the javascript file a JSON_Encoded object with the relevant values to carry on the 
+// last processes of the wilcoxon calculation. 
 $obj = new stdClass();
 $obj->label = "object";
 $obj->data = array(
